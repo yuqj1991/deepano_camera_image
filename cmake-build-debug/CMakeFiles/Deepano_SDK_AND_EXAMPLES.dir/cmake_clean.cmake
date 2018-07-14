f8174f1a5454fe736310b410f60e1834a88f406c
yuqj1990 <yuqj@deepano.com>
2018-07-14 20:48:40 +0800

    ...

--- cmake-build-debug/CMakeFiles/Deepano_SDK_AND_EXAMPLES.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Deepano_SDK_AND_EXAMPLES.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/Deepano_SDK_AND_EXAMPLES.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Deepano_SDK_AND_EXAMPLES.
-# This may be replaced when dependencies are built.
